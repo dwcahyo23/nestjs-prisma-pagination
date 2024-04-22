5c67c4a69fa2269f684ea71e6651e5d5a0331f79
Yusuf Dwi <dwcahyo23@gmail.com>
2024-04-22 16:24:21 +0700

    changes zero base

--- dist/prisma/search-paginator.js
+++ dist/prisma/search-paginator.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.searchPaginator = void 0;
-const searchPaginator = (defaultOptions) => {
-    return async (prisma, modelName, options) => {
-        let data;
-        if (options?.searchValue) {
-            data = await prisma.$queryRawUnsafe(`
+const searchPaginator = (defaultOptions) => async (prisma, modelName, options) => {
+    let data;
+    if (options?.searchValue) {
+        data = await prisma.$queryRawUnsafe(`
         SELECT *,
         (SELECT COUNT(*) FROM "${modelName}"
           WHERE to_tsvector('english',
@@ -18,29 +17,28 @@ const searchPaginator = (defaultOptions) => {
           limit ${options.perPage}
           offset ${options.skip};
       `);
-        }
-        else {
-            data = await prisma.$queryRawUnsafe(`
+    }
+    else {
+        data = await prisma.$queryRawUnsafe(`
          SELECT *,
         (SELECT COUNT(*) FROM "${modelName}") as row_count
         FROM "${modelName}"
         limit ${options.perPage}
         offset ${options.skip};
         `);
-        }
-        const total = Number(data[0]?.row_count || 0);
-        const lastPage = Math.ceil((total) / (options?.perPage || defaultOptions.perPage));
-        return {
-            data: data,
-            meta: {
-                total,
-                lastPage,
-                currentPage: options?.page || 1,
-                perPage: options?.perPage || 10,
-                prev: options?.page > 1 ? (options?.page || defaultOptions.page) - 1 : null,
-                next: options?.page < lastPage ? (options?.page || defaultOptions.page) + 1 : null,
-            },
-        };
+    }
+    const total = Number(data[0]?.row_count || 0);
+    const lastPage = Math.ceil((total) / (options?.perPage || defaultOptions.perPage) - 1);
+    return {
+        data: data,
+        meta: {
+            total,
+            lastPage,
+            currentPage: options?.page || 0,
+            perPage: options?.perPage || 10,
+            prev: options?.page > 0 ? (options?.page || defaultOptions.page) - 1 : null,
+            next: options?.page < lastPage ? (options?.page || defaultOptions.page) + 1 : null,
+        },
     };
 };
 exports.searchPaginator = searchPaginator;
