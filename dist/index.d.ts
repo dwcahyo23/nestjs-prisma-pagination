1ea7ad7b02af32bddd5276a294b785e4dd3ea373
Yusuf Dwi <dwcahyo23@gmail.com>
2025-04-16 14:12:39 +0700

    update nest

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { searchPaginator } from './prisma/search-paginator';
-import { paginator } from './prisma/paginator';
-import { getPagination, getPaginatedResult } from './prisma/get-paginated-result';
-import { PaginatorTypes } from '../index';
-export { searchPaginator, paginator, PaginatorTypes, getPagination, getPaginatedResult, };
